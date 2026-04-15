c9ffab3e61a2f6d9bcb289c9aa0415366f1e5fc9
Kim Branzell <hej@jagheter.kim>
2026-04-15 19:20:33 +0200

    Quartz sync: Apr 15, 2026, 7:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "Kim Branzell",
+    pageTitleSuffix: " - Kim Branzell",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "jagheter.kim",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
